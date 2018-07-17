1d6ba9b117eab470edd789d24eb795ef8e4802ca
eoli001 <Eoli@Yings-MacBook-Air.local>
2018-07-16 22:51:13 -0400

    fix

--- compressedstring.cpp
+++ compressedstring.cpp
@@ -37,9 +37,54 @@ string outputCompressString(string s){
 }
 
 int main() {
+
   string s = "aabc";
   s = outputCompressString(s);
-  cout<<s <<endl;
+  cout<< s <<endl;
+  string s1 = "aabcC";
+  s1 = outputCompressString(s1);
+  cout << s1 <<endl;
+
+  string s2 = "";
+  s2 = outputCompressString(s2);
+  cout << s2 <<endl;
+
+  string s3 = "aaaaab";
+  s3 = outputCompressString(s3);
+  cout << s3 <<endl;
+  
+  string s4 = " ";
+  s4 = outputCompressString(s4);
+  cout << s4 <<endl;
+
+  string s5 = "aaaaaa";
+  s5 = outputCompressString(s5);
+  cout << s5 <<endl;
+
+  string s6 = "aaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbb";
+  s6 = outputCompressString(s6);
+  cout << s6 <<endl;
+
+  string s7 = "abcabca";
+  s7 = outputCompressString(s7);
+  cout << s7 <<endl;
+
+  string s8 = "aabbcc";
+  s8 = outputCompressString(s8);
+  cout << s8 <<endl;
+
+  string s9 = "abccab";
+  s9 = outputCompressString(s9);
+  cout << s9 <<endl;
+
+  string s10 = "abbbb";
+  s10 = outputCompressString(s10);
+  cout<< s10 <<endl;
+
+  string s11 = "   ";
+  s11 = outputCompressString(s11);
+  cout<< s11 <<endl;
+
   return 0;
 }
 
